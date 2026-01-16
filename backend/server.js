d2ba8e2d9780dfd2d478df447e5159799fbe416e
Onyekachukwu Aniakor <carlosaniakorchukwu@gmail.com>
2026-01-16 10:47:01 +0000

    first commit

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ import adminOrderRoutes from './routes/adminOrderRoutes.js'
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors());
 
 dotenv.config();
 connectDB();
